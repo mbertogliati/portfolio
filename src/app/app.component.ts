e967aa01ab844f647aca4f039823b0f93c298241
Mateo Bertogliati <mbertogliati@frba.utn.edu.ar>
2023-07-24 10:44:20 -0300

    angular initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '@core/navbar/navbar.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'portfolio';
+}
