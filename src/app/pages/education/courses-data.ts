b113997b297ec8b5d550fbd5fe9eadc01ac65ff3
Mateo Bertogliati <mbertogliati@frba.utn.edu.ar>
2024-03-18 12:33:27 -0300

    Updated courses descriptions

--- src/app/pages/education/courses-data.ts
+++ src/app/pages/education/courses-data.ts
@@ -10,16 +10,37 @@ export const coursesData: Course[] = [
         iconURL: "/assets/img/skills/frameworks/laravel.png",
         year: "2022",
         description: `
-        I've learned the basics of this framework, including:<br>
+        This was my first contact with web development. I learned the basics of HTML, CSS, JavaScript, PHP, and MySQL.
+        <br>
+        The final project consisted of forming a team and developing a web application using the Laravel framework about a topic of our choice.
+        <br>
+        We chose a general purpose forum, where users can create posts, comment on them, and like or dislike comments.
+        <br>
+        The concepts included in the course were:
         <ul>
-            <li>Controllers</li>
-            <li>Services</li>
-            <li>Repositories</li>
-            <li>Routing</li>
-            <li>HTTP Requests</li>
-            <li>Forms</li>
-            <li>Middleware</li>
-        </ul>
+            <li>Lightweight front-end with Blade template engine
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                </ul>
+            </li>
+
+            <li>Backend with Laravel
+                <ul>
+                    <li>Routing</li>
+                    <li>Controllers</li>
+                    <li>Models</li>
+                    <li>Views</li>
+                    <li>Middleware</li>
+                    <li>Validation</li>
+                    <li>Authentication</li>
+                    <li>Authorization</li>
+                    <li>Eloquent ORM</li>
+                </ul>
+            </li>
+            <br>
+            You can find the forum repository <a href="https://github.com/mbertogliati/foro">here<a>
         `
     },
     {
@@ -27,16 +48,8 @@ export const coursesData: Course[] = [
         iconURL: "/assets/img/skills/python.png",
         year: "2021",
         description: `
-        I've learned the basics of this language, including:<br>
-        <ul>
-            <li>Variables</li>
-            <li>Loops</li>
-            <li>Functions</li>
-            <li>Classes</li>
-            <li>Modules</li>
-            <li>Libraries</li>
-            <li>APIs</li>
-        </ul>
+        This course was my first dive into Python. I've learned the basics of the languages and some of its most famous
+        libraries such as NumPy, Pandas, and Matplotlib.
         `
     }
 
